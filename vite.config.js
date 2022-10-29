315927b8047c7e86c76fee4a3da2fc643a2b399c
Nicolas Endrizzi <nicolasendrizzi@gmail.com>
2022-10-29 11:31:07 -0300

    removed configuration

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,10 @@
 import { defineConfig } from 'vite';
 import path from 'path';
 import react from '@vitejs/plugin-react';
-import { VitePluginFonts } from 'vite-plugin-fonts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePluginFonts({
-      google: {
-        families: ['Raleway'],
-      },
-    }),
-  ],
+  plugins: [react()],
   base: '/portfolio/',
   resolve: {
     alias: {
